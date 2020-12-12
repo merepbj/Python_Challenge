baa7194f3cc9fbfb880c23334607024217a0821f
merepbj <meredithjohnson76@gmail.com>
2020-12-12 12:51:28 -0500

    completed work

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,60 +1,67 @@
-#Your task is to create a Python script that analyzes the records to calculate each of the following:
-#Import Library
-import os
+# Dependencies
 import csv
-
-# Path to collect data from the Resources folder
-budget_csv = os.path.join('..', 'Resources', 'budget_data.csv')
-
-# Define the function and have it accept the 'budget_data' as its sole parameter
-
-  #* The greatest increase in profits (date and amount) over the entire period
-
-  #* The greatest decrease in losses (date and amount) over the entire period
+import os
+# Files to load and output (Remember to change these)
+file_to_load = os.path.join("Resources", "budget_data.csv")
 
 
+total_months = 0
+total_profit = 0
+previousNum = 0
+profit_losses = []
+sum_of_pl = 0
+net_change = 0
+greatest_increase = 0
+increase_month = ""
+greatest_decrease = 0
+decrease_month = ""
+# Read the csv and convert it into a list of dictionaries
+with open(file_to_load) as financial_data:
+    reader = csv.reader(financial_data)
+    # Read the header row
+    header = next(reader)
+    first_row = next(reader)
+    previousNum = int(first_row[1]) #867884
+    total_profit = int(first_row[1])
+    for row in reader:
+        total_months = total_months + 1
+        total_profit = total_profit + int(row[1])
+        #net-changes
+        net_change = previousNum - int(row[1])
+        profit_losses.append(int(row[1]) - previousNum) # -69417 - 310503
+        previousNum = int(row[1])
+         # -379920
+        if net_change > greatest_increase: # -379920 > 662642
+            greatest_increase = net_change
+            increase_month = row[0]
+        if net_change < greatest_decrease: # -379920 < -116771
+            greatest_decrease = net_change
+            decrease_month = row[0] # May-2010
+for x in profit_losses:
+    sum_of_pl = sum_of_pl + x
+print(total_months)
+print(profit_losses)
+print(sum_of_pl/len(profit_losses))
+print(total_profit) #38382578
+print(max(profit_losses))
+print(min(profit_losses))
+print(greatest_increase, increase_month)
+print(greatest_decrease, decrease_month)
+print("-------------------------")
 
-#In addition, your final script should both print the analysis to the terminal and export a text file with the results.
-# Read in the CSV file
-with open(budget_csv, 'r') as csvfile:
 
-    # Split the data on commas
-    csvreader = csv.reader(csvfile, delimiter=',')
-
-    header = next(csvreader)
-
-
-    # Prompt the user for what record they would like to search for
-    # record_to_check = input("What record do you want to look for? ")
-    count = 0
-
-    # Loop through the data
-    for row in csvreader:
-        count += 1
-
-    # pl is the total net sum of profit and losses
-    pl = 0
-    for row in csvreader:
-        pl += int(row[1])
-
-    # adds all values in the second column to an array so we can analyze easier
-    # this contains all the values in the second column
-    profit_loss = []
-    for row in csvreader:
-        profit_loss.append(int(row[1]))
-
-    # record the difference between the next row and the current row
-    change = 0
-    differences = []
-    for i in range(len(profit_loss) - 1):
-        change = profit_loss[i + 1] - profit_loss[i]
-        differences.append(change)
-
-    # to find sum of everything inside differences (dsum) and then find average
-    dsum = 0
-    for i in differences:
-        dsum += i
-
-    average = dsum/86
-        
-      
+# Specify the file to write to
+output_path = os.path.join("Analysis", "new.txt")
+# Open the file using "write" mode. Specify the variable to hold the contents
+with open(output_path, 'w', newline='', encoding = "UTF-8") as txtfile:
+    print(total_months)
+    print(profit_losses)
+    print(sum_of_pl/len(profit_losses))
+    print(total_profit) #38382578
+    print(max(profit_losses))
+    print(min(profit_losses))
+    print(greatest_increase, increase_month)
+    print(greatest_decrease)
+    print("-------------------------")
+    print(increase_month)
+    print(decrease_month)
